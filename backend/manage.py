576472537c508814503c741241155a7b60c1c959
Pegoral123 <jhonathanpegoral123@gmail.com>
2026-04-28 19:39:16 -0300

    refactor: Organizar importações e configuração do WSGI no manage.py e wsgi.py

--- backend/manage.py
+++ backend/manage.py
@@ -1,18 +1,16 @@
-import pymysql
-pymysql.install_as_MySQLdb()
 import os
 import sys
 
+import pymysql
+pymysql.install_as_MySQLdb()
+
 
 def main():
-  
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed?"
-        ) from exc
+        raise ImportError("Couldn't import Django.") from exc
     execute_from_command_line(sys.argv)
 
 
